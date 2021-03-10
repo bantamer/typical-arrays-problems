e5a07528a12dd2fdb8ec4ef9171a9529eb560265
bantamer <pazhitnykhpetr@gmail.com>
2021-03-10 15:41:59 +0300

    feat: add prettier

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
+};
 
-exports.min = function min (array) { 
-  if (!array || array.length === 0){
-    return 0;
-  }
-  return Math.min.apply(null,array);
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
+};
 
-
-exports.max = function max (array) {
-  if (!array || array.length === 0){
-    return 0;
-  }
-  return Math.max.apply(null,array);
-}
-
-exports.avg = function avg (array) {
-  if (!array || array.length === 0){
-    return 0;
-  } else { 
-  const reducer = (acc,curVal) => (acc+curVal)
-  let sum = array.reduce(reducer);
-  let lenArr = array.length
-  return sum/lenArr;
-  };
-}
-
-
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        const reducer = (acc, curVal) => acc + curVal;
+        let sum = array.reduce(reducer);
+        let lenArr = array.length;
+        return sum / lenArr;
+    }
+};

8620661770ac7d1bee8b39ec3cbfb04f8d5815d2
bantamer <pazhitnykhpetr@gmail.com>
2021-03-10 14:47:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) { 
+  if (!array || array.length === 0){
+    return 0;
+  }
+  return Math.min.apply(null,array);
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  return Math.max.apply(null,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  } else { 
+  const reducer = (acc,curVal) => (acc+curVal)
+  let sum = array.reduce(reducer);
+  let lenArr = array.length
+  return sum/lenArr;
+  };
 }
+
+
